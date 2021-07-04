92d697000c587d63a821c922ecb46d0443833615
Navaneeth170688 <navanee.bsc@gmail.com>
2021-07-04 17:18:09 +0530

    adding_update to first py file

--- first_prog.py
+++ first_prog.py
@@ -1,6 +1,10 @@
+#Day1 in training
+
+#to print a statement
 print("This is my fist python program")
 print("This is my fist python program")
 
+#adding numbers with combintion of arithmetic operators
 print(5+6)
 print(5+6*5)
 print(5+2*5)
@@ -21,19 +25,23 @@ print(2**3+2*3)
 print((2**3)+2*3)
 print((2**3+2)*3)
 
+#Printing integer
 doorno=1511
 print(doorno)
 
-
+#Printing name
 name = 'navaneeth'
 name = "navaneeth"
 print(name)
 
+#using numbers in variable_name
 tamil1 = 90
 print(tamil1)
 tamil2 = 60
 print(tamil2)
 
+
+#using underscore in variable_name
 tamil_1 = 21
 print(tamil_1)
 tamil_2 = 31
@@ -45,6 +53,7 @@ print(TAMIL1)
 TAMIL = 51
 print(TAMIL)
 
+#Using underscore as a first character in variable
 _Tamil = 61
 print(_Tamil)
 
